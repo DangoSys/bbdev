400172eebf83fd9ee1bbf9f0612fbd73d4a5cdc6
daiyongyuan <1533208939@qq.com>
2026-08-13 20:52:44 +0800

    refactor: remove Verilator batch simulation option

--- api/tests/test_api_bebop_verilator_run.py
+++ api/tests/test_api_bebop_verilator_run.py
@@ -1,3 +1,3 @@
 from _api_test_helper import run_bbdev_case
 
-run_bbdev_case("bbdev bebop-verilator --run '--diff --batch --no-wave --jobs 16 --binary toy_im2col_k3_test-singlecore-baremetal --config sims.verilator.BuckyballToyVerilatorConfig'")
+run_bbdev_case("bbdev bebop-verilator --run '--diff --no-wave --jobs 16 --binary toy_im2col_k3_test-singlecore-baremetal --config sims.verilator.BuckyballToyVerilatorConfig'")
